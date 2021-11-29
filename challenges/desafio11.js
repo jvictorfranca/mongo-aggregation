b454a81da86a1d8197d52aaae870d5cf5f62e106
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-11-29 16:36:13 -0300

    tenta de novo o 10

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,16 @@
-"oi";
+db.trips.aggregate([
+  { $addFields: {
+    day: { $dayOfWeek: "$startTime" },
+  } },
+  { $group: {
+    _id: "$day",
+    count: { $sum: 1 },
+  } },
+  { $sort: { _count: -1 } },
+  { $limit: 1 },
+  { $project: {
+    diaDaSemana: "$_id",
+    total: "$count",
+    _id: false,
+  } },
+]);
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -21,3 +21,25 @@ db.air_routes.aggregate([
   { $sort: { totalRotas: -1 } },
   { $limit: 1 },
 ]);
+
+// use('aggregations')
+// db.air_routes.aggregate([
+//   {$match: {
+//     airplane: {$in:["747", "380"]}
+//   }},
+//   {$lookup: {
+//     from: "air_alliances",
+//     let: {airline_name: "$airline.name"},
+//     pipeline: [
+//       {$unwind: "$airlines"},
+//       {
+//         $match:{
+//           $expr: {
+//         $eq: ["$airlines", "$$airline_name"]
+//       }}}
+
+//     ],
+//     as: "airlineData"
+//   }},
+//   {$group: {_id:"$airlineData.name", count: {$sum:1}}}
+// ])
